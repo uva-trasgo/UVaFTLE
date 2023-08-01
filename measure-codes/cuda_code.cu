bfa934fc2a0a33949dc9a49a30618df37b90492f
Rocío Carratalá Sáez <rcarrata@uji.es>
2023-08-01 19:53:44 +0200

    Update cuda_code.cu
    Remove old typedefs not used anymore

--- measure-codes/cuda_code.cu
+++ measure-codes/cuda_code.cu
@@ -7,27 +7,6 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-typedef struct Face {
-   int      index;      /* index to mesh->faces structure */
-   int     *vertices;   /* index to mesh->points structure */
-} face_t;
-
-typedef struct Point {
-   int       index; // Point index in mesh->points
-   double   *coordinates;
-   int       nfaces;
-   int      *faces;
-} point_t;
-
-typedef struct Mesh {
-   int       nDim;
-   int       nPoints;
-   int       nFaces;
-   int       nVertsPerFace;
-   point_t  *points;
-   face_t   *faces;
-} mesh_t;
-
 void read_coordinates ( char *filename, int nDim, int nPoints, double *coords )
 {
  int ip, d, check_EOF;
